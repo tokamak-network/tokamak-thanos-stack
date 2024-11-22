60a3dcbcb55678ae82bf53d600b0182e9805da13
ohbyeongmin <fbottlem@gmail.com>
2024-11-22 11:50:42 +0900

    [OR-1947] add k8s setting

--- terraform/thanos-stack/thanos-stack.tf
+++ terraform/thanos-stack/thanos-stack.tf
@@ -8,6 +8,16 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+data "terraform_remote_state" "secretsmanager" {
+  backend = "s3"
+
+  config = {
+    bucket = var.backend_bucket_name
+    key    = "tokamak-thanos-stack/terraform/init/secretsmanager/terraform.tfstate"
+    region = var.aws_region
+  }
+}
+
 module "eks" {
   source = "./modules/eks"
 
@@ -55,4 +65,22 @@ module "rds" {
       apply_method = "pending-reboot"
     }
   ]
+}
+
+module "k8s" {
+  source = "./modules/kubernetes"
+
+  network_name                       = var.thanos_stack_name
+  profile                            = "default"
+  region                             = var.aws_region
+  vpc_id                             = data.terraform_remote_state.vpc.outputs.vpc_id
+  cluster_name                       = module.eks.cluster_name
+  fargate_profiles                   = module.eks.fargate_profiles
+  cluster_endpoint                   = module.eks.cluster_endpoint
+  cluster_certificate_authority_data = module.eks.cluster_certificate_authority_data
+  cluster_oidc_issuer_url            = module.eks.cluster_oidc_issuer_url
+  oidc_provider_arn                  = module.eks.oidc_provider_arn
+  aws_acm_certificate_validation     = module.acm.aws_acm_certificate_validation
+  aws_secretsmanager_id              = data.terraform_remote_state.secretsmanager.outputs.aws_secretsmanager_id
+  external_secret_namespace          = var.thanos_stack_name
 }
