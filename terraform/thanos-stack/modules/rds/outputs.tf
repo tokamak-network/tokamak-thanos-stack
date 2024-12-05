903a9afb9b3996e5bacb0ccabc97afc7b26b4e57
ohbyeongmin <fbottlem@gmail.com>
2024-12-05 14:35:39 +0900

    [OR-1959] add rds outputs

--- terraform/thanos-stack/modules/rds/outputs.tf
+++ terraform/thanos-stack/modules/rds/outputs.tf
@@ -1,3 +1,11 @@
 output "rds_endpoint" {
   value = aws_db_instance.default.endpoint
 }
+
+output "rds_address" {
+  value = aws_db_instance.default.address
+}
+
+output "rds_port" {
+  value = aws_db_instance.default.port
+}
