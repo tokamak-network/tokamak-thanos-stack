f11e2de7554cb984f513819798ca50c4b26d42e4
ohbyeongmin <fbottlem@gmail.com>
2025-01-02 17:35:24 +0900

    [OR-2017] del route53

--- terraform/init/route53/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "name_servers" {
-  description = "Domain name servers"
-  value       = aws_route53_zone.main.name_servers
-}
--- terraform/init/route53/route53.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route53_zone" "main" {
-  name          = var.chain_domain_name
-  force_destroy = true
-}
