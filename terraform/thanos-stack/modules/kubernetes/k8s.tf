1ae268e776d796291bbce4daa279b1be82d1f518
Nam Pham <nam@tokamak.network>
2025-04-08 10:26:17 +0000

    feat: set MAX_CHANNEL_DURATION on op-batcher

--- terraform/thanos-stack/modules/kubernetes/k8s.tf
+++ terraform/thanos-stack/modules/kubernetes/k8s.tf
@@ -196,6 +196,7 @@ resource "terraform_data" "thanos_stack_values" {
       stack_rollup_file_url        = var.stack_rollup_file_url
       stack_op_geth_image_tag      = var.stack_op_geth_image_tag
       stack_thanos_stack_image_tag = var.stack_thanos_stack_image_tag
+      stack_max_channel_duration   = var.stack_max_channel_duration
     }
   }
 
